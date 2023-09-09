b7f6ced555e2ac9a1d509a89a96c0e14d41fe649
Nikorasu00 <141564145+Nikorasu00@users.noreply.github.com>
2023-09-09 17:09:52 +1000

    make some changes to the endpoint

--- routes/eventCategory-api.js
+++ routes/eventCategory-api.js
@@ -2,9 +2,9 @@ const express= require("express")
 const eventCatCont = require("../controller/stats")
 const router = express.Router();
 router.post("/33349800/api/v1/createEventCategory", eventCatCont.createEventCat)
-router.get("/33349800/api/v1/list-event", eventCatCont.getAll)
-router.get("/33349800/api/v1/delete-event", eventCatCont.deleteEventCatById)
-router.put("/33349800/api/v1/add-event", eventCatCont.addEvent)
+router.get("/33349800/api/v1/list-eventCat", eventCatCont.getAll)
+router.get("/33349800/api/v1/delete-eventCat", eventCatCont.deleteEventCatById)
+router.put("/33349800/api/v1/add-eventCat", eventCatCont.addEvent)
 router.post("/33349800/api/v1/delete-eventCat", eventCatCont.deleteEventCatById)
-router.post("/33349800/api/v1/update-event", eventCatCont.updateEventCatById)
+router.post("/33349800/api/v1/update-eventCat", eventCatCont.updateEventCatById)
 module.exports = router;
